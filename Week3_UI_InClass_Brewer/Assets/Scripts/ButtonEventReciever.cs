8ce7f34762bbea249838add9c5f433e2a7c820eb
codbrew <cb535114@scc-mmlab-13.local>
2019-01-29 17:30:13 -0500

    WholeProjectUpload

---
+++ Week3_UI_InClass_Brewer/Assets/Scripts/ButtonEventReciever.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEventReciever : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // This is a fuction
+    public void OnButtonPress()
+    {
+        // Outputs a string to the console
+        Debug.Log("Button Pressed");
+    }
+}
